c9a327eac03e7ffdba8a800e27dfe4c8731db949
Ayokunnumi1 <omololuayk@gmail.com>
2023-08-08 10:45:40 +0100

    Create dynamic booklist

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -31,3 +31,46 @@ function getFromLocal() {
   }
 }
 
+function createDynamicBooks() {
+  getFromLocal();
+  listContainer.innerHTML = '';
+  booksData.forEach((book, index) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('parent-list');
+
+    const contentContainer = document.createElement('div');
+    contentContainer.classList.add('content-container');
+
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('title-container');
+    contentContainer.appendChild(titleContainer);
+
+    const bookName = document.createElement('h2');
+    bookName.classList.add('book-name');
+    bookName.textContent = book.title;
+    titleContainer.appendChild(bookName);
+
+    const authorContainer = document.createElement('div');
+    authorContainer.classList.add('author-container');
+    contentContainer.appendChild(authorContainer);
+
+    const authorName = document.createElement('p');
+    authorName.classList.add('author-name');
+    authorName.textContent = book.author;
+    authorContainer.appendChild(authorName);
+
+    const btnContainer = document.createElement('div');
+    btnContainer.classList.add('btn-container');
+
+    const removeBTN = document.createElement('button');
+    removeBTN.textContent = 'Remove';
+    removeBTN.classList.add('remove-btn');
+    removeBTN.addEventListener('click', () => removeBook(index));
+    btnContainer.appendChild(removeBTN);
+
+    listItem.appendChild(contentContainer);
+    listItem.appendChild(btnContainer);
+
+    listContainer.appendChild(listItem);
+  });
+}
